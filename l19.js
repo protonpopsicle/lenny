9292cbc431f4bbae6cc3f3b6f70e5fc8fcfad785
Scott Meisburger <smeisburger@gmail.com>
2025-04-07 15:08:01 -1000

    updated l19 bot

--- l19.js
+++ l19.js
@@ -30,6 +30,13 @@ async function initializeVocab() {
     }
 }
 
+function toTitleCase(str) {
+    return str.toLowerCase().split(' ').map(word => {
+        if (word.length === 0) return word;
+        return word[0].toUpperCase() + word.slice(1);
+    }).join(' ');
+}
+
 function n(plural = false) {
     let item = vocab.nouns.next();
     if (Math.random() < 0.15) {
@@ -68,22 +75,22 @@ function adj() {
 
 async function speak() {    
     const templates = [
-        () => `It seems that you have been living two lives. In one life, you are ${n().charAt(0).toUpperCase() + n().slice(1)}, ${n()} ${v(2)} for a respectable ${n()} company. You have a ${n()}, you ${v()} your ${n()} and you help your land lady ${v()} her ${n()}.`,
+        () => `It seems that you have been living two lives. In one life, you are ${toTitleCase(n())}, ${n()} ${v(2)} for a respectable ${n()} company. You have a ${n()}, you ${v()} your ${n()} and you help your land lady ${v()} her ${n()}.`,
         () => `You ${v()} like they do. I've never seen anyone ${v()} that ${adj()}.`,
         () => `Most of my ${n()} you already ${v()}.`,
         () => `I know why you wanna ${v()} me. 'Cuz ${n()} is all the world's even seen lately.`,
         () => `You can ${v()} that ${n()} through this life if you want but you cant ${v()} that ${n()} off that ${n()} (No sir).`,
-        () => `${n().charAt(0).toUpperCase() + n().slice(1)} ${['II', 'III', 'IV', 'V', 'VI'][Math.floor(Math.random() * 5)]}: The Sequel to ${n().charAt(0).toUpperCase() + n().slice(1)}`,
+        () => `${toTitleCase(n())} ${['II', 'III', 'IV', 'V', 'VI'][Math.floor(Math.random() * 5)]}: The Sequel to ${toTitleCase(n())}`,
         () => `Well well well, if it isn't the ${adj()} ${n()} Nicholas.`,
         () => `Can you store the ${adj()} ${n()} with air? Or not`,
         () => `The shepherd ${v(1)} at ${adj()} ${n()}, whose withered ${n()} had just reached into the coat pocket. "Why have you come here?" uttered the shepherd, trying to ignore the ${n()} level reading he glanced from the scanner. "I'm here for ${n()}19".`,
         () => `In ${adj()}, ${adj()} prose, she ${v(1)} how a beloved star was exposed as a violent ${n()}, and how ${n()}'s conviction became the first of the ${n()} era.`,
         () => `No ${n()} could stop them. In their ${adj()} pride, ${('the ' + n(true) + ' of ' + n()).toUpperCase()} ${v(1)} on their commander's base like ${n(true)} in the winds of ${n()}.`,
-        () => `The ${n()} of Shokam's ${adj()} ${n()} let loose on top of ${n().charAt(0).toUpperCase() + n().slice(1)} creek. Blood ${v(1)} down on the soldiers as they ${v(1)} in their huts. "${n().charAt(0).toUpperCase() + n().slice(1)}", said the Lieutenant.`,
-        () => `I've seen ${n(true)} you people wouldn't believe. ${adj()} ${n(true)} on fire off the ${n()} of Orion. I watched C-${n(true)} ${v()} in the dark near the ${n().charAt(0).toUpperCase() + n().slice(1)}häuser Gate. All those moments will be lost in time, like tears in the ${n()}. Time to die.`,
+        () => `The ${n()} of Shokam's ${adj()} ${n()} let loose on top of ${toTitleCase(n())} creek. Blood ${v(1)} down on the soldiers as they ${v(1)} in their huts. "${toTitleCase(n())}", said the Lieutenant.`,
+        () => `I've seen ${n(true)} you people wouldn't believe. ${adj()} ${n(true)} on fire off the ${n()} of Orion. I watched C-${n(true)} ${v()} in the dark near the ${toTitleCase(n())}häuser Gate. All those moments will be lost in time, like tears in the ${n()}. Time to die.`,
         () => `I am quite ${adj()}. Look at those ${n(true)}. The boys want to get the ${n(true)} into bed so they can have their ${n()} ${n(true)} off their ${n()} and forth. When a ${n()} ${v()}s his ${n()} you don't call it love. Why get all misty-${n()}'d when a ${n()} ${v()}s another part of his anatomy?`,
         () => `In the hospital men's room, as I'm ${v(3)} my ${n(true)}, I glance in the mirror. The ${n()} I see is not so much me as my ${n()}. When did he show up? There is no soap; I rub ${n()} sanitizer into my ${n()}--it burns. I nearly ${v()} myself in the sink trying to rinse it off.`,
-        () => `The question is in a way ${adj()}, she ${v(3)}, but one must ask. ${n().charAt(0).toUpperCase() + n().slice(1)} in such situations is rarely ${adj()}. Sex is the engine, exalting and ruining people, sex and frustration. ${n().charAt(0).toUpperCase() + n().slice(1)} is what people believe is worth the path of devastation.`
+        () => `The question is in a way ${adj()}, she ${v(3)}, but one must ask. ${toTitleCase(n())} in such situations is rarely ${adj()}. Sex is the engine, exalting and ruining people, sex and frustration. ${toTitleCase(n())} is what people believe is worth the path of devastation.`
     ];
 
     let item = templates[Math.floor(Math.random() * templates.length)]();
@@ -91,7 +98,6 @@ async function speak() {
     if (Math.random() < 0.2) {
         item = item + "\n" + `Neo: That's why it's going to ${v()}.`;
     }
-    
     return item;
 }
 
